352276c141928cc3acce1edafec8584b30eca4a8
Huang Bo <huangbop@gmail.com>
2016-10-16 17:52:25 +0800

    With .jsbeautifyrc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 $(function() {
-    $('div.poem-stanza').addClass('highlight');
+
+  $('#switcher-default').addClass('selected');
+  $('#switcher button').click(function() {
+    var bodyClass = this.id.split('-')[1];
+    console.log(bodyClass);
+    $('body').removeClass().addClass(bodyClass);
+    $('#switcher button').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
 });
